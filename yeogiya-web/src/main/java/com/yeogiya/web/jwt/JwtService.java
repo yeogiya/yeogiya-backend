e0c60f3529c24cb067bcc9b5b3cbcff113eb7f44
ijnooyah <ijnooyah@gmail.com>
2023-07-22 10:41:26 +0900

    :hammer: PR 코멘트 JWT 코드 반영

--- yeogiya-web/src/main/java/com/yeogiya/web/jwt/JwtService.java
+++ yeogiya-web/src/main/java/com/yeogiya/web/jwt/JwtService.java
@@ -64,7 +64,7 @@ public class JwtService {
 
     /**
      * RefreshToken 생성
-     * RefreshToken은 Claim에 email도 넣지 않으므로 withClaim() X
+     * RefreshToken은 Claim에 id도 넣지 않으므로 withClaim() X
      */
     public String createRefreshToken() {
         Date now = new Date();
@@ -118,19 +118,19 @@ public class JwtService {
     }
 
     /**
-     * AccessToken에서 Email 추출
+     * AccessToken에서 ID 추출
      * 추출 전에 JWT.require()로 검증기 생성
      * verify로 AceessToken 검증 후
-     * 유효하다면 getClaim()으로 이메일 추출
+     * 유효하다면 getClaim()으로 아이디 추출
      * 유효하지 않다면 빈 Optional 객체 반환
      */
-    public Optional<String> extractEmail(String accessToken) {
+    public Optional<String> extractId(String accessToken) {
         try {
             // 토큰 유효성 검사하는 데에 사용할 알고리즘이 있는 JWT verifier builder 반환
             return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
                     .build() // 반환된 빌더로 JWT verifier 생성
                     .verify(accessToken) // accessToken을 검증하고 유효하지 않다면 예외 발생
-                    .getClaim(ID_CLAIM) // claim(Emial) 가져오기
+                    .getClaim(ID_CLAIM) // claim(id) 가져오기
                     .asString());
         } catch (Exception e) {
             log.error("액세스 토큰이 유효하지 않습니다.");
