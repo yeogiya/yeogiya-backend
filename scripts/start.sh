47c5550f521520ddc138d2e359b85d76c7aeabe5
geniushyeon <dev.geniushyeon@gmail.com>
2023-10-04 23:07:51 +0900

    🔧 start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,9 +13,14 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
+source /home/ec2-user/deploy/env/yeogiya.env
+echo $JASYPT_PASSWORD
+
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar \
+          -Dspring.profiles.active=local \
+          -Djasypt.encryptor.password=$JASYPT_PASSWORD \ > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
