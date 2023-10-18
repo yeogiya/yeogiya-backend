c5d1d0b823f658d58733d9c8bf58f81535bc60db
geniushyeon <dev.geniushyeon@gmail.com>
2023-10-18 22:08:16 +0900

    🐛 배포 스크립트에서 jar 파일 실행 권한 추가, 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,12 +16,16 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 source /home/ec2-user/deploy/env/yeogiya.env
 echo $JASYPT_PASSWORD
 
+# 실행 권한 추가
+echo "> $JAR_FILE 에 실행 권한 추가"
+chmod +x $JAR_FILE
+
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar \
           -Dspring.profiles.active=local \
           -Djasypt.encryptor.password=$JASYPT_PASSWORD \
-          $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
+          $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
