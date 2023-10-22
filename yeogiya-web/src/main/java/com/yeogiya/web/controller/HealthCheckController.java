8195e03de8a3bc74951a48fec3319bd494712640
geniushyeon <dev.geniushyeon@gmail.com>
2023-10-22 19:30:33 +0900

    🐛 healthCheck API 버그 수정

--- yeogiya-web/src/main/java/com/yeogiya/web/controller/HealthCheckController.java
+++ yeogiya-web/src/main/java/com/yeogiya/web/controller/HealthCheckController.java
@@ -3,16 +3,15 @@ package com.yeogiya.web.controller;
 import com.yeogiya.web.logger.HealthCheckLogger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class HealthCheckController {
 
     private final DataSource dataSource;
