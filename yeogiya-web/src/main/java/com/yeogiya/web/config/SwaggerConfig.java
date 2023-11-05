9a595c7d5fbadeed4005e76c1a39e6f4b8ef0fe1
ijnooyah <ijnooyah@gmail.com>
2023-11-05 11:11:39 +0900

    ✨ jwt 인증 스웨거 추가

--- yeogiya-web/src/main/java/com/yeogiya/web/config/SwaggerConfig.java
+++ yeogiya-web/src/main/java/com/yeogiya/web/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.yeogiya.web.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,8 +18,21 @@ public class SwaggerConfig {
                 .version("v1")
                 .description("여기야 프로젝트 API 문서");
 
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
