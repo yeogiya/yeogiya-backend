413bcd9d88a40da08451a5cd333dd7c408212dbf
geniushyeon <dev.geniushyeon@gmail.com>
2023-12-21 22:52:17 +0900

    ✨ 장소 검색 API 추가

--- yeogiya-web/src/main/java/com/yeogiya/web/YeogiyaApplication.java
+++ yeogiya-web/src/main/java/com/yeogiya/web/YeogiyaApplication.java
@@ -3,7 +3,7 @@ package com.yeogiya.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @EntityScan(basePackages = {"com.yeogiya"})
 @EnableJpaRepositories(basePackages = {"com.yeogiya"})
+@EnableFeignClients
 public class YeogiyaApplication {
 
 	public static void main(String[] args) {
---
+++ yeogiya-web/src/main/java/com/yeogiya/web/search/feign/client/KakaoSearchClient.java
@@ -0,0 +1,18 @@
+package com.yeogiya.web.search.feign.client;
+
+import com.yeogiya.web.search.feign.dto.KakaoSearchResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "kakaoSearchUrl", url = "${search.kakao.url}")
+public interface KakaoSearchClient {
+
+    @GetMapping
+    KakaoSearchResponseDTO searchPlaceByKeyword(
+            @RequestHeader String Authorization,
+            @RequestParam String query,
+            @RequestParam int page,
+            @RequestParam int size);
+}
